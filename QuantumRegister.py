a9f6d971f6594d9bedd552120b11d40af784f554
BenET01 <s2055565@ed.ac.uk>
2023-02-28 15:45:21 +0000

    added docstrings and added to grovers algorithm

--- QuantumRegister.py
+++ QuantumRegister.py
@@ -4,20 +4,42 @@ import numpy as np
 import math
 
 class QuantumRegister:
+    """
+    Class which simulates a Quantum Register
+
+    Class Attributes: 
+        n_qubits = number of qubits to initialise class with
+        state = which state the class is initialised into, as an integer, set to 0
+    """
     def __init__(self, n_qubits, state = 0):
+        """
+        Initialises the QuantumRegister class with: 
+            n_states = 2 ** n_qubits
+            state = np.zeros(n_states, dtype = complex)
+        """
         self.n_qubits_ = n_qubits
         self.n_states_ = 2**self.n_qubits_
         self.state_ = np.zeros(self.n_states_, dtype = complex)
         self.state_[state] = 1 
+        # 2x2 matrix which applies a hadamard gate
         self.hadamard = np.array(([1, 1], [1, -1])) / math.sqrt(2)
+        # 2x2 matrix which applies a NOT gate
         self.NOT = np.array(([0, 1], [1, 0])) 
+        # 2x2 matrix which applies a T gate
         self.T = np.array(([1, 0], [0, complex(np.cos(math.pi/4), np.sin(math.pi/4))]))
+        # 2x2 matrix which applies a T dagger gate
         self.T_dagger = np.array(([1, 0], [0, complex(np.cos(-math.pi/4), np.sin(-math.pi/4))]))
         
     def setEqualSuperposition(self):
+        """
+        Function which makes the register equally likely to be found in any state
+        """
         self.state_ = np.ones(self.n_states_, dtype = complex) / math.sqrt(self.n_states_)
     
     def measureState(self):
+        """
+        Function which collapses the wavefunction, and so the register goes into a singular state
+        """
         prob = np.abs((self.state_))**2
         index = np.random.choice(self.n_states_, p=prob)
         self.state_ = np.zeros(self.n_states_, dtype = complex)
@@ -26,6 +48,15 @@ class QuantumRegister:
 
 
     def applyGate(self, gate, target, control = None):
+        """
+        Function that applies an arbitrary gate to a specified qubit, with an optional control qubit
+        :type gate: np.ndarray
+        :param gate: A 2x2 matrix which operates on a qubit
+        :type target: int
+        :param target: index of the target qubit
+        :type control: int
+        :param control: index of the control qubit, if not None
+        """
         assert gate.shape == (2, 2), f"Gate Matrix has wrong dimensions, please input a 2x2 array \n input array was of shape {gate.shape}"
         assert type(target) == int and target < self.n_qubits_, f"Target Qubit is outwith range of Qubits \nnumber of qubits was initialised as {self.n_qubits_}, however target qubit was {target}"
         if control != None:
@@ -48,6 +79,13 @@ class QuantumRegister:
 
 
     def toffoli(self, target, control):
+        """
+        Function to apply the toffoli (controlled-controlled Not Gate) to a target qubit
+        :type target: int
+        :param target: index of the target qubit
+        :type conrol: tuple
+        :param control: tuple of the index's of the two control qubits
+        """
         assert type(control) == tuple and len(control) == 2, "Number of Control qubits needs to be a tuple of length 2"
         assert type(target) == int and type(control[0]) == int and type(control[1]) == int, "qubits need to be given as integers"
         c0 = control[0]
@@ -69,6 +107,13 @@ class QuantumRegister:
         self.applyGate(gate = self.NOT, target = c0, control = c1)
 
     def contrZ(self, target, control):
+        """
+        Function which applies the controlled Z gate onto a target qubit, using a control qubit
+        :type target: int
+        :param target: Index of the target qubit
+        :type control: int
+        :param control: Index of the control qubit
+        """
         assert type(target) == int and type(control) == int, "Target or control is not of type int"
         assert target < self.n_qubits_, f"Target Qubit is outwith range of Qubits \nnumber of qubits was initialised as {self.n_qubits_}, however target qubit was {target}"
         assert control < self.n_qubits_, f"Control Qubit is outwith range of Qubits \nnumber of qubits was initialised as {self.n_qubits_}, however control qubit was {control}"
--- grovers.py
+++ grovers.py
@@ -4,8 +4,21 @@ import Operators as op
 import QuantumRegister as qr
 
 class Grovers:
+    """
+    Class which implements Grover's Algorithm 
+
+    Attributes: 
+        oracle: A function which returns 1 for a chosen item, or 0 otherwise
+        list: A list to search 
+    """
 
     def __init__(self, oracle, list):
+        """
+        Initialises Grovers class with: 
+            n_qubits_ = log2(len(list))
+            register = QuantumRegister(n_qubits)
+            n_states_ = 2 ** n_qubits
+        """
         self.oracle = oracle
         self.n_qubits_ = math.ceil(math.log2(len(list))) 
         self.register = QuantumRegister(self.n_qubits_)
@@ -22,6 +35,12 @@ class Grovers:
         mu = (1/self.n_states_) * np.sum(self.register.state_)
         for i in range(register.n_states_):
             self.register.state_[i] = self.register.state_[i] - 2*mu
+
+    def groversAlgorithm(self):
+        self.register.setEqualSuperposition()
+        for i in range(int((math.pi/4) * math.sqrt(self.n_states_) - 0.5)):
+            self.register.groversOneIteration()
+        
 
     def findIndex(self):
         index = self.register.measureState()
